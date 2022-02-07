a6424a96308365df0fc7e75e95775161a6679c6d
Chackoseb <95061303+Chackoseb@users.noreply.github.com>
2022-02-07 23:48:50 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,15 @@
+function changeToRed(){
+    document.body.style.backgroundColor="red";
+}
+
+function changeToBlue(){
+    document.body.style.backgroundColor="blue";
+}
+
+function changeToYellow(){
+    document.body.style.backgroundColor="yellow";
+}
+
+function changeToBlack(){
+    document.body.style.backgroundColor="black";
+}
